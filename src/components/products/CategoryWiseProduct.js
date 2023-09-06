d85081375ba47ed01b0193f0f9f17f6e7b0ca0d8
gayathri-16 <gayathri.p973@gmail.com>
2023-09-06 14:34:50 +0530

    category file update

--- src/components/products/CategoryWiseProduct.js
+++ src/components/products/CategoryWiseProduct.js
@@ -5,18 +5,18 @@ import { Link } from 'react-router-dom';
 // import Card from './Card'
 
 import {getAdminCategories} from '../../actions/categoryAction';
-
+import { getProducts } from '../../actions/productActions';
 import {getProductByFilter} from '../../actions/filterAction'
+
 
 function CategoryWiseProduct(props) {
  
 
-
-    const{categories = []}=useSelector((state) => state.categoryState)   
-    const{filterproducts = []}=useSelector((state)=>state.filterState)
+    
+   
     // const [text, setText] =useState('');
     const[categoryIds, setCategoryIds] = useState([]);
-
+    const[text, setText]=useState('')
     const dispatch = useDispatch();
 
 
@@ -28,18 +28,21 @@ function CategoryWiseProduct(props) {
     },[dispatch])
 
 
-
+  useEffect(()=>{
+    dispatch(getProducts())
+  },[dispatch])
 
  
-
-  
+  const{categories = []}=useSelector((state) => state.categoryState)   
+  const{filterproducts = []}=useSelector((state)=>state.filterState)
+  const {products = []}=useSelector((state)=>state.productsState)
     // const handleSearch = e =>{
     //     setCategoryIds(e.target.value);
     //     dispatch(getProductByFilter({ type: 'category', query:e.target.value}))
     // };
 
     const handleCategory = e => {
-      resetState();
+      // resetState();
   
       const currentCategoryChecked = e.target.value;
       const allCategoriesChecked = [...categoryIds];
@@ -63,11 +66,11 @@ function CategoryWiseProduct(props) {
     };
    
 
-    const resetState = () =>
-    {
+    // const resetState = () =>
+    // {
   
-    setCategoryIds([])
-    }
+    // setCategoryIds([])
+    // }
     return (
     <>
         <div className="container" style={{width:"120%"}}>
@@ -81,7 +84,7 @@ function CategoryWiseProduct(props) {
 
               <div  className='category_area'>
 
-            <div id="check_category"> <img src={categories[0]?.images[0]?.image}    alt=""/><h3 className='categoryname'>{categories[0]?.category}</h3><input type="checkbox" value={categories[0]?._id}   checked={categoryIds.includes(categories[0]?._id)} onChange={handleCategory}/></div>
+              <div id="check_category"> <img src={categories[0]?.images[0]?.image}    alt=""/><h3 className='categoryname'>{categories[0]?.category}</h3><input type="checkbox" value={categories[0]?._id}   checked={categoryIds.includes(categories[0]?._id)} onChange={handleCategory}/></div>
 
               </div>
               <div className='category_area'>
@@ -122,6 +125,57 @@ function CategoryWiseProduct(props) {
 		
         </div> */}
        
+<div className="container" style={{width:"120%"}}>
+         <div className="special-products">
+            <div className="page-header">
+           <h2>GIFTS</h2>
+          </div>
+      <div className="special-products-pro" >
+        <div className="slider-items-products">
+
+          <div id="special-products-slider" className="product-flexslider hidden-buttons">
+            <div className="category-grid">
+   
+              {products&& products.map(product =>(
+                <div className="product-item">
+               
+                <div className="item-inner" style={{width:"250px",height:"330px",marginLeft:"1rem"}}>
+                  <div className="product-thumb has-hover-img"  > 
+                   <Link to={`/product/${product._id}`}> <figure style={{border: "1px #eee solid"}}>{product.images.length > 0 && <> <img src={product.images[0]?.image} alt="" /> <img className="hover-img" src={product.hoverimages[0]?.image} alt="" /> </> }</figure></Link>   
+                <div className="pr-info-area animated animate2"><a href="quick_view.html" className="quick-view"><i className="fa fa-search"><span>Quick view</span></i></a> <a href="wishlist.html" className="wishlist"><i className="fa fa-heart"><span>Wishlist</span></i></a> <a href="compare.html" className="compare"><i className="fa fa-exchange"><span>Compare</span></i></a> </div>
+                  </div>
+                  <div className="item-info" >
+                    <div className="info-inner" >
+                  
+                      <div className="item-content item-details">
+                      <div className="item-title">
+                        <h4><a title="Ipsums Dolors Untra" href="single_product.html">{product.name}</a></h4>
+                      </div>
+                      <div className='ratings mt-auto'>
+                         <div className='rating-outer'>
+                         <div className='rating-inner'  style={{width: `${product.ratings / 5 * 100}%` }}></div>
+                         </div>
+                     </div>                        
+                      <div className="item-price">
+                          <div className="price-box"> <span className="regular-price"> <span className="price">₹ {product.price}</span> </span> </div>
+                        </div>
+                        <div className="pro-action">
+                          <button type="button" className="add-to-cart-mt"> <i className="fa fa-shopping-cart"></i><span> Add to Cart</span> </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               ))}
+            
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+        </div> 
+
         {/* <div className="container" style={{width:"120%"}}>
          <div className="special-products">
             <div className="page-header">
