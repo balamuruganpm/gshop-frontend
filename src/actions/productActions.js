cfe99d20d85a3d8d7f26b1e7907ad48821e62bbe
gayathri-16 <gayathri.p973@gmail.com>
2023-09-09 11:24:09 +0530

    action file

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -9,9 +9,9 @@ export const getProducts = (price) => async (dispatch)=>{
     try{
          dispatch(productsRequest())
          let link = `${process.env.REACT_APP_URL}/api/v1/products`
-        //  if(price){
-        //     link += `?price[gte]=${price[0]}&price[lte]=${price[1]}`
-        //  }
+         if(price){
+            link += `?price[gte]=${price[0]}&price[lte]=${price[1]}`
+         }
          const {data} =  await axios.get(link);
 
          dispatch(productsSuccess(data))
